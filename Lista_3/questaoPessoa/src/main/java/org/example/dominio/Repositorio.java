9d1afaf43c4b25a558434207904985151b61500f
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-10-13 23:25:45 -0300

    Delete Lista_3/questaoPessoa directory

--- Lista_3/questaoPessoa/src/main/java/org/example/dominio/Repositorio.java
+++
@@ -1,8 +0,0 @@
-package org.example.dominio;
-
-public interface Repositorio {
-    public void guarda(Pessoa nova);
-    public Pessoa recupera(String cpf);
-    public Pessoa primeiro();
-    public Pessoa proximo();
-}
