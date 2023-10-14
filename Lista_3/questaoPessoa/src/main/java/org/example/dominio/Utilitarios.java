80c06ecd743246956f2b9bb1ac66a19e94954dbb
João Vitor <83173020+joaovitor73@users.noreply.github.com>
2023-10-13 23:26:53 -0300

    questaoPessoa no case 4 apresenta falhas

---
+++ Lista_3/questaoPessoa/src/main/java/org/example/dominio/Utilitarios.java
@@ -0,0 +1,31 @@
+package org.example.dominio;
+
+import java.util.ArrayList;
+
+public class Utilitarios {
+
+    public void duplica(Turma t1,Turma t2){
+        ArrayList<Pessoa> alunos = t1.getAlunos();
+        for(Pessoa p : alunos ){
+            t2.guarda(p);
+        }
+
+    }
+
+    public void diferenca(Turma t1, Turma t2, Turma t3){
+        ArrayList<Pessoa> alunos1 = t1.getAlunos();
+        ArrayList<Pessoa> alunos2 = t2.getAlunos();
+        boolean flag;
+        for(Pessoa a1: alunos1){
+            flag = true;
+            for(Pessoa a2: alunos2){
+                if(a1.equals(a2)) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) t3.guarda(a1); //a1 não existe em alunos 2
+        }
+
+    }
+}
